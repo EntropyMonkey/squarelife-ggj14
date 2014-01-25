848212c908c1d2c005ab1ebf39bd70a868648d22
Andrea Distler <andrea.distler@gmail.com>
2014-01-25 12:13:09 +0100

    added player prefab

---
+++ Unity/Assets/Scripts/IMovement.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// an interface for different movement classes
+/// </summary>
+public interface IMovement
+{
+
+}
---
+++ Unity/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// The Player's movement component
+/// </summary>
+public class PlayerMovement : MonoBehaviour, IMovement
+{
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
